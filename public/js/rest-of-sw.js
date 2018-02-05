f019993a796be72b657766cea4aee72f732ef99e
James Maciver <j.maciver22@gmail.com>
2018-02-05 22:03:10 +0000

    add web push to distibute announcements

--- public/js/rest-of-sw.js
+++ public/js/rest-of-sw.js
@@ -48,6 +48,90 @@ self.addEventListener('activate', event => {
     event.waitUntil(openDb());
 });
 
+const serviceWorkerUrl = new URL(self.location);
+
+self.addEventListener('fetch', event => {
+    console.log(`[ServiceWorker:fetch] Fetch Event: `, event);
+    
+    const requestUrl = event.request.url;
+    const urlBelongsToOrigin = requestUrl.indexOf(serviceWorkerUrl.origin) > -1;
+    const isStaticResourceRequest = requestUrl.indexOf('/api/') === -1;
+    
+    if (urlBelongsToOrigin) {
+
+        const isGETRequest = event.request.method === 'GET';
+        const isPOSTRequest = event.request.method === 'POST';
+        
+        if(isGETRequest) {
+
+            if (isStaticResourceRequest) {
+                console.log(`[ServiceWorker:fetch] Processing static resource fetch event.`);
+
+                event.respondWith(
+                    applyStaticResourceCachingStrategy(event)
+                );
+            } else {
+                console.log(`[ServiceWorker:fetch] Processing dynamic data fetch event`, event);
+                
+                const accessingSessions = requestUrl.indexOf('/api/sessions') > -1;
+                
+                let promiseToResolve;
+        
+                if (accessingSessions) {
+                    promiseToResolve = getSessions(event.request);
+                } else {
+                    promiseToResolve = fetch(event.request);
+                }
+                event.respondWith(
+                    promiseToResolve
+                );
+            }
+        } else if (isPOSTRequest) {
+            console.log(`[ServiceWorker:fetch] Processing data POST fetch event.`, event);
+            event.respondWith(fetch(event.request));
+        }
+    }
+});
+
+const getSessions = (request) => {
+    return getSessionsFromDb().then((resultSet) => {
+        if(resultSet && resultSet.length > 0) {
+            return returnAsJsonResponse(resultSet);
+        }
+        return fetch(request)
+                .then(response => response.json())
+                .then(sessions => 
+                {
+                    const promises = [];
+                    sessions.forEach(session => {
+                        promises.push(addSessionToDb(session));
+                    });
+                    return Promise.all(promises).then(() => {
+                        return returnAsJsonResponse(sessions);
+                    });
+                });
+    });
+}
+
+const getSessionsFromDb = () => {
+    return openDb()
+        .then(db => 
+                db.transaction('sessions')
+                    .objectStore('sessions')
+                    .getAll()
+            );
+}
+
+const addSessionToDb = (session) => {
+    return openDb()
+        .then(db =>
+                db.transaction('sessions', 'readwrite')
+                    .objectStore('sessions')
+                    .put(session)
+                    .complete
+            );
+}
+
 const applyStaticResourceCachingStrategy = (event) => {
     return caches.open(`v1`).then(cache => {
         return cache.match(event.request).then(cacheResponse => {
@@ -60,14 +144,19 @@ const applyStaticResourceCachingStrategy = (event) => {
 }
 
 const openDb = () => {
-    return idb.open('dddscot', 1, (upgradeDB) => {
+    return idb.open('dddscot', 2, (upgradeDB) => {
         console.log(`[ServiceWorker:activate] Migrating db from  v${upgradeDB.oldVersion}}.`);
             
         if(!upgradeDB.objectStoreNames.contains('sessions')){
             upgradeDB.createObjectStore('sessions', {
                 keyPath: 'title'
             });
-        }       
+        }   
+        if(!upgradeDB.objectStoreNames.contains('announcements')){
+            upgradeDB.createObjectStore('announcements', {
+                keyPath: 'timestamp'
+            });
+        }     
     });
 }
 
